dd32c9ec3d8162e7aa3da40d5cd74c789a4edb23
etztrefis <etztrefishello@gmail.com>
2020-09-04 15:57:08 +0700

    some changes idk

--- cron.js
+++ cron.js
@@ -10,30 +10,30 @@ const bot = new VkBot({
 });
 
 console.log("Cron Started.");
+try {
+  const connection = mysql.createPool({
+    connectionLimit: 15,
+    host: process.env.SERVERNAME,
+    user: process.env.USERNAME,
+    database: process.env.DBNAME,
+    password: process.env.PASSWORD,
+  });
 
-const connection = mysql.createPool({
-  connectionLimit: 15,
-  host: process.env.SERVERNAME,
-  user: process.env.USERNAME,
-  database: process.env.DBNAME,
-  password: process.env.PASSWORD,
-});
+  let now = new Date();
+  let dayOfWeek = now.getDay() + 1;
+  let year = now.getFullYear();
+  let month = now.getMonth();
+  let mday = now.getDate() + 1;
 
-let now = new Date();
-let dayOfWeek = now.getDay() + 1;
-let year = now.getFullYear();
-let month = now.getMonth();
-let mday = now.getDate() + 1;
+  let hardDays = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  if (hardDays.includes(mday)) {
+    mday = "0" + mday;
+  }
+  if (hardDays.includes(month)) {
+    month = "0" + month;
+  }
 
-let hardDays = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-if (hardDays.includes(mday)) {
-  mday = "0" + mday;
-}
-if (hardDays.includes(month)) {
-  month = "0" + month;
-}
-
-let query = `SELECT 
+  let query = `SELECT 
   Dishes.Name, Dishes.Price, Dishes.EnergyValue
 FROM
   eaterymain.Menu,
@@ -42,11 +42,10 @@ WHERE
   Menu.DayOfWeek = 3 AND
   Dishes.DishID = Menu.DishID
   `;
-connection.query(query, dayOfWeek, function (err, result) {
-  if (err) {
-    console.log(err.message);
-  }
-  try {
+  connection.query(query, dayOfWeek, function (err, result) {
+    if (err) {
+      console.log(err.message);
+    }
     if (result !== 0) {
       let row = [
         result[0].Name,
@@ -85,18 +84,18 @@ connection.query(query, dayOfWeek, function (err, result) {
         }
       });
     }
-  } catch (e) {
-    console.error(e);
-    connection.query("SELECT UID FROM Users", function (UIDerror, usersResult) {
-      if (UIDerror) {
-        console.log(UIDerror.message);
-      }
-      for (let i = 0; i < usersResult.length; i++) {
-        bot.sendMessage(
-          usersResult[i].UID,
-          "Записей о меню на завтрашний день в базе не найдено. 🚫"
-        );
-      }
-    });
-  }
-});
+  });
+} catch (e) {
+  console.error(e);
+  connection.query("SELECT UID FROM Users", function (UIDerror, usersResult) {
+    if (UIDerror) {
+      console.log(UIDerror.message);
+    }
+    for (let i = 0; i < usersResult.length; i++) {
+      bot.sendMessage(
+        usersResult[i].UID,
+        "Записей о меню на завтрашний день в базе не найдено. 🚫"
+      );
+    }
+  });
+}
